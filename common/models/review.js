b5db7f4e8e5d7a936ff1b8a6d8715d7a4275b720
Ziad Saab <ziad.saab@gmail.com>
2015-10-23 17:07:57 +0000

    Working file upload to S3 :)

--- common/models/review.js
+++ common/models/review.js
@@ -4,6 +4,8 @@ var app = require('../../server/server');
 var secret = process.env.CAPTCHA_KEY;
 // var multiparty = require('multiparty');
 // var loopback = require('loopback');
+var fs = require('fs');
+var storage = app.datasources.reviewImages;
 
 function checkCaptch(parsedData, cb) {
     request.post({
@@ -60,108 +62,19 @@ function randomString() {
 
 module.exports = function(Review) {
 
-        Review.createNew = function(data, req, res, cb) {
-            // console.log(req)
-            // Review.createNew = function(req, res, cb) {
-            // console.log("before parse: ", req)
+        Review.createNew = function(data, req, cb) {
+            var randomDir = 'files_' + Math.random().toString(36);
+            var file = req.files.fileUpload;
+            var fileName = randomDir + '/' + file.originalFilename;
+            var upStream = app.models.container.uploadStream('breeder-review-images', fileName, {});
+            var fileStream = fs.createReadStream(file.path);
             
-            // var form = new multiparty.Form();
-            
-            // form.parse(req);
-            // var ds = Review.app.models.container.dataSource
-            
-            // console.log(data)
-            // console.log("req")
-            // console.log(req)
-            // console.log(req.files.type)
-            // var datasources = Review.dataSource;
-            // console.log(req.files.fileUpload)
-            var parsedData = JSON.parse(data);
-            
-            // console.log("data")
-            // console.log(parsedData)
-            // console.log(parsedData.fileUpload)
-            // var storageService = new StorageService()
-            // var Container = app.models.container;
-            // console.log(Container)
-            // console.log(parsedData)
-            // checkCaptch(parsedData, cb);
-            // parsedData.images = [];
-            // console.log(req)
-            // console.log(req.files.fileUpload)
-            // console.log(req.length)
-            // if (req.files.fileUpload.type.indexOf('image') < 0) {
-            //     cb(new Error("file is not an image"));
-            // }
-            // if (req.files.fileUpload.size > 10485760) {
-            //     cb(new Error("file is larger than 10MB"));
-            // }
-
-            // req.files.fileUpload.name = randomString();
-            // console.log(app.datasources['rate-my-dog-breeder-review-images'])
-            // console.log(Object.keys(app.datasources['rate-my-dog-breeder-review-images']))
-            // app.datasources['rate-my-dog-breeder-review-images']
-            
-        
-            app.datasources.rateMyDogBreederReviewImages.connector.client.upload(req, res, {container: 'xxxx'}, function(err, result) {
-                        // ds.upload(req, res, {container: 'xxxx'}, function(err, result) {
-
-                console.log('---------', err, result);
+            upStream.on('finish', function() {
+                // THIS IS WHERE YOU CREATE THE REVIEW :)
+                cb(null, 'OK');
             })
+            fileStream.pipe(upStream);
             
-            
-            
-            //  var Container = Review.app.models.container;
-            // //  console.log(Container)
-            // //  console.log("req files")
-            // //  console.log(req.files.fileUpload.headers)
-            // //  req.files.fileUpload.headers.headers = req.headers
-            // //  console.log(req.files.fileUpload)
-            //  Container.getContainer({name:'breeder-review-container-test'},function(err,c) {
-            //     //  console.log(c.name)
-             
-            // //  Container.getContainer({container: "rate-my-dog-breeder-review-images"},function(err,c) {
-            // Container.upload(req, res,{container: 'breeder-review-container-test'}, function(err, res) {
-            //     console.log('-----------', arguments);
-            //     if (err) {
-            //         cb(err);
-            //     }
-            //     else {
-            //         cb();
-            //     }
-            // })
-            //  })
-            //     });
-            // }
-            //     Container.upload(req,res,{container: c.name},cb)
-            // });
-            //  var storage     =   require('loopback-component-storage');
-            // var storageService  = storage.StorageService({provider: 'filesystem', root: '/tmp'});
-                // Container.createContainer({name:'breeder-review-images-1'},function(err,c) {
-                // Container.upload(req, res, {container: "rate-my-dog-breeder-review-images"}, cb)
-                // function(err, response) {
-
-                //  Container.upload(req,res,{container: c.name},cb)
-                // });
-            // Container['rate-my-dog-breeder-review-images'].upload(req, res, {container: 'breeder-review-images'}, function(err, response) {
-                // if (err) {
-                //     console.log(err)
-                //     return cb(new Error("upload Error: " + err))
-                // }
-
-                // console.log(response);
-
-
-
-                // // if (app.datasources['fkwehfkw'].provider === 'amazon') {
-                // //     decodedData.images.push('http://s3.blabla/' + containerName + '/' + fileName);
-                // // }
-                // // else if (app.datasource['fkehfke'].provider === 'local') {
-                // //     decodeddata.images.push('/images/' + fileName);
-                // // }
-                // Review.create(parsedData, cb);
-            // });
-                // });
         }
 
         Review.remoteMethod('createNew', {
@@ -180,12 +93,6 @@ module.exports = function(Review) {
                 type: 'object',
                 http: {
                     source: 'req'
-                }
-            }, {
-                arg: 'res',
-                type: 'object',
-                http: {
-                    source: 'res'
                 }
             }],
             returns: {
